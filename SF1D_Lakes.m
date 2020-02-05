a8d2840581615a55ca97007704701e326340f8af
jbuffo <33759152+jbuffo@users.noreply.github.com>
2020-02-05 17:13:16 -0500

    Add files via upload

---
+++ SF1D_Lakes.m
@@ -0,0 +1,278 @@
+%% This is the main control file for simulating the evolution of shallow lake solidification
+%  and will be used with 'one_D_Adv_ARD_interactive_lakes.m',
+%  'one_D_advective_flux_lakes.m', and 'Thomas_Trid.m'. The majority of
+%  modifications will be made to the input parameters and the output will
+%  be Temperature, Porosity, Salinity, and Bulk Salinity profiles.
+%  (variables - Temperature, Liquid_Fraction, Salinity,
+%  Porosity'.*Salinity)
+
+%% Input Parameters 
+%% Change these as desired - these govern the majority of the results
+Ttop=240;                %% Ice top temp (K) (use T_surf below if you want evolving surface temp)
+Tbottom=267.78;          %% Ocean Temp (K) [MgSO4 12.3ppt-273.0K, 100ppt-271.31K, 282ppt-267.5K NaCl 34ppt-271.2K
+                         %%                  NaCl 100ppt - 267.78, NaCl 200ppt - 261.08] *always put above liquidus*
+Tm=273.15;               %% Melt temperature of pure ice (K)
+k_i=2;                   %% Thermal conductivity (ice) W/m*K
+k_br=.6;                 %% '                            ' (brine)
+k_s=2*10^-9;             %% Diffusivity for Salt
+dt=100;                  %% Time step (s)
+tf=10000000;             %% Final time (s)
+dz=.001;                 %% Spatial discretization (m)
+H=0.20;                  %% Domain size (m)
+c_br=3985;               %% Specific heat of seawater (J/K)
+c_i=2000;                %% '              ' ice
+L=334774;                %% Latent heat of fusion ice<->water (J/Kg)
+L_epsom=13744870;        %% Latent heat of formation for precipitated salts (epsomite - 13744870) (J/Kg)
+S_bottom=100;            %% Ocean Salinity (ppt)
+S_sat=357;               %% Ocean Saturation (ppt) NaCl - 357
+rho_i=917;               %% Density of Ice (Kg/m^3)
+rho_br=1081;             %% Density of Ocean (used in volume averaging - 1D grav. drainage uses delta S) 34ppt NaCl-1027
+                         %  100ppt NaCl-1081 200ppt NaCl-1168 12.3ppt MgSO4-1012, 100pppt-1103, 282ppt-1323
+STol=0.1;                %% Error Tolerance for Salinity
+PhiTol=0.005;            %% Error Tolerance for Porosity
+TTol=0.01;               %% Error Tolerance for Temperature
+m=2;                     %% Cementation exponent
+g=9.8;                   %% Earth Gravity
+mu=1.88*10^-3;           %% Viscosity of Ocean
+phi_c=0.05;              %% Critical Porosity
+
+%% Sinusoidal surface temp, BE SURE TO CHANGE 'Ttop' in the calling of 'one_D_adv_ARD_interactive_lakes.m'
+%  to 'T_surf(n)' if using this
+
+T_surf=273.15-15*sin(2*pi*[1:dt:31557600]/31557600);  %% Sinusoidal surface temp
+
+
+%% Bottom Boundary Condition options Open=1 or Closed=2 bottom boundary (Salt)
+%BCoc=1;
+BCoc=2;
+S_0=H*S_bottom;
+%% Dirichlet=1 or Neumann=2 bottom boundary condition (Temp)
+%BCdn=1;
+BCdn=2;
+
+dT=0.025;      %% Temperature gradient at bottom boundary (K/m) - geothermal flux
+
+%% Plotting Vector
+Depth=[0:-dz:-H];
+
+%% Initial Condition Vectors for Phi and T  and S
+    T_initial=0*[0:dz:H]+Tbottom;
+    S_initial=0*[0:dz:H]+S_bottom;
+    phi_initial=0*[0:dz:H]+1;
+
+%% Vectors for function iterations
+Temperature=[];
+Liquid_Fraction=[];
+Salinity=[];
+
+T_hold=[];
+Phi_hold=[];
+Sal_hold=[];
+
+Salt_Precip=0;
+Salt_Layer=0;
+
+%% Looping Over Time
+for n=1:tf/dt
+    if n==1;
+
+        [NewTemp,NewPhi,NewS]=one_D_adv_ARD_interactive_lakes(...
+        k_i,k_br,k_s,dt,dz,H,c_i,c_br,L,STol,TTol,PhiTol,phi_initial,...
+        T_initial,S_initial,S_bottom,rho_i,...
+        rho_br,Tm,m,T_surf(n),Tbottom,g,rho_br,phi_c,BCoc,BCdn,dT);
+
+        Temperature=NewTemp;
+        Liquid_Fraction=NewPhi;
+        Salinity=NewS;
+        
+        % Marking the ice-ocean interface
+        IO_int=0;
+        if BCoc==2
+            for i=1:length(NewPhi)
+                if NewPhi(i)<0.99
+                    IO_int=IO_int+1;
+                else
+                end
+            end
+            
+            % Calculating salinity of remaining brine due to gravity
+            % drainage
+            S_ice=dz*sum(Liquid_Fraction(1:IO_int).*Salinity(1:IO_int));
+            S_br=S_0-S_ice;
+            S_ppt=S_br/((length(NewPhi)-IO_int)*dz);
+            Salinity(IO_int+1:end)=S_ppt;
+            
+            % Salt precipitation and accompanying heat of formation
+            if S_ppt>S_sat
+                S_hold=(S_ppt-S_sat)*(length(NewPhi)-IO_int);
+                Salt_Precip=Salt_Precip+S_hold;
+                Salt_Layer=floor(Salt_Precip/1000);
+                Salinity(IO_int+1:end)=S_sat;
+                % Temperature mod
+                del_T=((L_epsom/c_br)*(S_hold/1000))/(length(NewPhi)-IO_int);
+                Temperature(IO_int+1:end)=Temperature(IO_int+1:end)+del_T;
+                if Salt_Layer==length(NewPhi)-IO_int
+                    break
+                else
+                end
+            else
+            end
+        else
+        end
+
+    else
+        [NewTemp,NewPhi,NewS]=one_D_adv_ARD_interactive_lakes(...
+        k_i,k_br,k_s,dt,dz,H,c_i,c_br,L,STol,TTol,PhiTol,Liquid_Fraction',...
+        Temperature',Salinity',S_bottom,rho_i,...
+        rho_br,Tm,m,T_surf(n),Tbottom,g,rho_br,phi_c,BCoc,BCdn,dT);
+        
+        
+        Temperature=NewTemp;
+        Liquid_Fraction=NewPhi;
+        Salinity=NewS;
+        
+        if ismember(n*dt,[0:100000:10000000])==1
+            T_hold=[T_hold Temperature];
+            Phi_hold=[Phi_hold Liquid_Fraction];
+            Sal_hold=[Sal_hold Salinity];
+        else
+        end
+        
+        IO_int=0;
+        if BCoc==2
+            for i=1:length(NewPhi)
+                if NewPhi(i)<0.99
+                    IO_int=IO_int+1;
+                else
+                end
+            end
+            
+            % Calculating salinity of remaining brine due to gravity
+            % drainage
+            S_ice=dz*sum(Liquid_Fraction(1:IO_int).*Salinity(1:IO_int));
+            S_br=S_0-S_ice;
+            S_ppt=S_br/((length(NewPhi)-IO_int)*dz);
+            Salinity(IO_int+1:end)=S_ppt;
+            
+            % Salt precipitation and accompanying heat of formation
+            if S_ppt>S_sat
+                S_hold=(S_ppt-S_sat)*(length(NewPhi)-IO_int);
+                Salt_Precip=Salt_Precip+S_hold;
+                Salt_Layer=floor(Salt_Precip/1000);
+                Salinity(IO_int+1:end)=S_sat;
+                % Temperature mod
+                del_T=((L_epsom/c_br)*(S_hold/1000))/(length(NewPhi)-IO_int);
+                Temperature(IO_int+1:end)=Temperature(IO_int+1:end)+del_T;
+                if Salt_Layer==length(NewPhi)-IO_int
+                    break
+                else
+                end
+            else
+            end
+        else
+        end
+%         if BCoc==2
+%             for i=1:length(NewPhi)
+%                 if NewPhi(i)<1
+%                     IO_int=IO_int+1;
+%                 else
+%                 end
+%             end
+%             S_ice=dz*sum(Liquid_Fraction(1:IO_int).*Salinity(1:IO_int));
+%             S_br=S_0-S_ice;
+%             S_ppt=S_br/((length(NewPhi)-IO_int)*dz);
+%             Salinity(IO_int+1:end)=S_ppt;
+%         else
+%         end
+        
+    end
+end
+
+% for i=1:length(Liquid_Fraction)
+%     if Liquid_Fraction(i)<=phi_c
+%         Porosity(i)=phi_c;
+%     else
+%         Porosity(i)=Liquid_Fraction(i);
+%     end
+% end
+
+%% Bulk salinity vs depth
+figure
+subplot(1,4,1)
+plot(T_hold,Depth)
+title('Temperature vs. Depth')
+xlabel('Temperature (K)')
+ylabel('Depth (m)')
+subplot(1,4,2)
+plot(Phi_hold,Depth)
+title('Porosity vs. Depth')
+xlabel('Porosity')
+ylabel('Depth (m)')
+subplot(1,4,3)
+plot(Sal_hold,Depth)
+title('Salinity vs. Depth')
+xlabel('Salinity (ppt)')
+ylabel('Depth (m)')
+xlim([0 max(Salinity)+5])
+% subplot(1,5,4)
+% plot(SinI,Depth)
+% title('Ice Salinity vs. Depth')
+% xlabel('Salinity (ppt)')
+% ylabel('Depth (m)')
+subplot(1,4,4)
+plot(Phi_hold.*Sal_hold,Depth)
+title('Bulk Salinity vs. Depth')
+xlabel('Bulk Salinity (ppt)')
+ylabel('Depth (m)')
+xlim([0 max(Salinity.*Liquid_Fraction)+5])
+
+figure
+subplot(1,4,1)
+plot(Temperature,Depth)
+title('Temperature vs. Depth')
+xlabel('Temperature (K)')
+ylabel('Depth (m)')
+subplot(1,4,2)
+plot(Liquid_Fraction,Depth)
+title('Porosity vs. Depth')
+xlabel('Porosity')
+ylabel('Depth (m)')
+subplot(1,4,3)
+plot(Salinity,Depth)
+title('Salinity vs. Depth')
+xlabel('Salinity (ppt)')
+ylabel('Depth (m)')
+xlim([0 max(Salinity)+5])
+% subplot(1,5,4)
+% plot(SinI,Depth)
+% title('Ice Salinity vs. Depth')
+% xlabel('Salinity (ppt)')
+% ylabel('Depth (m)')
+subplot(1,4,4)
+plot(Liquid_Fraction.*Salinity,Depth)
+title('Bulk Salinity vs. Depth')
+xlabel('Bulk Salinity (ppt)')
+ylabel('Depth (m)')
+xlim([0 max(Salinity.*Liquid_Fraction)+5])
+
+% velocity=[];
+% for i=2:length(vel_hold)
+%     velocity(i-1)=(vel_hold(i,1)-vel_hold(i-1,1))/(vel_hold(i,2)-vel_hold(i-1,2));
+% end
+% 
+% figure
+% plot(vel_hold(2:end,2),velocity)
+% title('Growth Velocity vs. Time')
+% xlabel('Growth Velocity (m/s)')
+% ylabel('Bulk Salinity (ppt)')
+
+% k_eff=(1/S_bottom)*salt_hold(2:end);
+% log_k_eff=log((1./k_eff)-1);
+% figure
+% plot(100*velocity,log_k_eff)
+% title('Growth Velocity vs. Log(Keff)')
+% xlabel('Growth Velocity (cm/s)')
+% ylabel('ln((1/Keff)-1)')
+
+% figure
+% plot([1:31557600],-15*sin(2*pi*[1:31557600]/31557600)+5*sin(2*pi*[1:31557600]/86400))
---
+++ one_D_adv_ARD_interactive_lakes.m
@@ -0,0 +1,196 @@
+function [T_np1_k_j,phi_np1_k_j,S_np1_k_j]=one_D_adv_ARD_interactive_lakes(...
+    k_i,k_br,k_s,dt,dz,H,c_i,c_br,L,STol,TTol,PhiTol,phi_initial,T_initial,...
+    S_initial,S_bottom,rho_i,rho_br,Tm,m,Ttop,Tbottom,g,rho_sw,phi_c,BCoc,BCdn,dT)
+
+%% Initial Condition Vectors for Phi, T, S, w
+phi_initial=phi_initial';
+T_initial=T_initial';
+phi_np1_km2_j=phi_initial;
+T_np1_km1_j=T_initial;
+S_initial=S_initial';
+S_np1_km1_j=S_initial;
+%SinI=SinI_initial;
+
+S_evolve=[]';
+T_evolve=[]';
+phi_evolve=[]';
+SErr=STol+1;
+TErr=TTol+1;
+PhiErr=PhiTol+1;
+counter=0;
+matrix_dimension=H/dz+1;
+
+
+%% Itterattions over k
+while SErr>STol || TErr>TTol || PhiErr>PhiTol;
+    counter=counter+1;
+    if counter>25
+        disp(counter)
+    else
+    end
+
+    %% Flux from gravity drainage parameterization
+    [T_source,S_source]=one_D_advective_flux_lakes(phi_np1_km2_j,S_np1_km1_j...
+        ,T_np1_km1_j,H,dz,dt,S_bottom,g,rho_sw);
+    
+    %% Freezing point depression calculations
+    %   delta_T=1.853*(S_np1_km1_j/28);        %% Old Freezing point depression
+    %   due to salt (linear approx)
+
+    %     delta_T=Tm-(-(1.333489497*(10^-5)*S_np1_km1_j.^2)-0.01612951864*S_np1_km1_j+...
+    %         273.055175687);                       %% Liquidus point using FREZCHEM Europa ocean
+    
+    delta_T=Tm-(-(9.1969758*(10^-5)*S_np1_km1_j.^2)-0.03942059*S_np1_km1_j+...
+       272.63617665);                        %% Liquidus point using FREZCHEM seawater
+
+    Hs=c_i*(Tm-delta_T);                     %% Enthalpy Calculation
+    Hsmelt=c_i*Tm;                           %% Enthalpy to melt ice
+
+    %% Calculating solid fraction at k-1 iteration for use in
+    %% temperature, salinity, and brine velocity at k iteration
+    
+    %% Value of Phi(n+1,k-1,j)
+    phi_np1_km1_j=phi_np1_km2_j;
+    for i=1:matrix_dimension;
+    %% Test to see if melting ice or freezing brine
+    En=c_i*T_np1_km1_j(i)+L*phi_np1_km2_j(i);
+        if En<Hs(i)+phi_c*L
+            phi_np1_km1_j(i)=phi_c;
+        elseif En>Hs(i)+L;
+            phi_np1_km1_j(i)=1;
+        else
+            phi_np1_km1_j(i)=((c_i*T_np1_km1_j(i)+phi_np1_km2_j(i)*L)-Hs(i))/...
+            L;
+        end;
+    end;
+
+    %% Reassigning Phi(n+1,k-2) as Phi(n+1,k-1) for next
+    %% iteration
+    phi_np1_km2_j=phi_np1_km1_j;
+    
+    
+    %% Create K matrix and solve for T
+    D=((phi_np1_km1_j*k_br+(1-phi_np1_km1_j)*k_i)*dt)./...
+        ((dz^2)*(phi_np1_km1_j*(rho_br*c_br)+(1-phi_np1_km1_j)...
+        *(rho_i*c_i)));
+    for i=1:matrix_dimension;
+            a(i)=1+2*D(i);
+            b(i)=-D(i);
+            c(i)=-D(i);
+    end        
+
+    b(end)=[];
+    c(1)=[];
+    y=T_initial-((rho_i*L./((phi_np1_km1_j*(rho_br*c_br)+...
+        (1-phi_np1_km1_j)*(rho_i*c_i)))).*...
+        (phi_np1_km1_j-phi_initial))+(1/((phi_np1_km1_j*(rho_br*c_br)+...
+        (1-phi_np1_km1_j)*(rho_i*c_i))))'.*T_source;
+
+    y(1)=y(1)+D(1)*Ttop;
+    
+    if BCdn==1
+        y(end)=y(end)+D(end)*Tbottom;
+    else
+        c(end)=c(end)-D(end-1);
+        y(end)=y(end)+2*D(end)*dz*dT;
+    end
+
+    % Solve for T
+    T_np1_k_j=Thomas_Trid(a,b,c,y');
+    
+    T_np1_km1_j=T_np1_k_j;
+    
+    %% Create K_s matrix and solve for S
+    one_over_phi1(1:matrix_dimension)=1;
+    one_over_phi=rdivide(one_over_phi1',phi_np1_km1_j);
+    %one_over_one_minus_phi=rdivide(one_over_phi1',1-phi_np1_km1_j);
+    for i=1:matrix_dimension;
+        if phi_np1_km1_j(i)<=phi_c;
+            one_over_phi(i)=0;
+        else
+        end;
+    end;
+%     for i=1:matrix_dimension;
+%         if one_over_one_minus_phi(i)==Inf;
+%             one_over_one_minus_phi(i)=0;
+%         end;
+%     end;
+    k_s_mod1(1:matrix_dimension)=k_s;
+    k_s_mod=times(k_s_mod1',phi_np1_km1_j.^m);
+    D_s_mod=((k_s_mod*dt)/dz^2).*one_over_phi;
+    
+    % Spatially averaged diffusion tensors
+%     D_s_plus=(D_s_mod+circshift(D_s_mod,1))/2;
+%     D_s_plus(1)=D_s_mod(1);
+%     D_s_minus=(D_s_mod+circshift(D_s_mod,-1))/2;
+%     D_s_minus(end)=D_s_mod(end);
+% 
+%     a2(1)=1+D_s_minus(1);
+%     b2(1)=-D_s_minus(1);
+%     for i=2:matrix_dimension;
+%             a2(i)=1+D_s_plus(i)+D_s_minus(i);
+%             b2(i)=-D_s_minus(i);
+%             c2(i)=-D_s_plus(i);
+%     end
+    
+    a2(1)=1+D_s_mod(1);
+    b2(1)=-D_s_mod(1);
+    for i=2:matrix_dimension;
+            a2(i)=1+D_s_mod(i)+D_s_mod(i);
+            b2(i)=-D_s_mod(i);
+            c2(i)=-D_s_mod(i);
+    end
+
+    b2(end)=[];
+    c2(1)=[];
+    y2=S_initial-(rho_i/rho_br)*one_over_phi.*S_initial...
+        .*(phi_np1_km1_j-phi_initial)+one_over_phi.*S_source;
+%     for i=1:length(phi_np1_km1_j)
+%         if phi_np1_km1_j(i)==0
+%             y2(i)=0;
+%             SinI(i)=one_over_one_minus_phi(i)*(SinI(i)*(1-phi_initial(i))-S_initial(i)*(phi_np1_km1_j(i)-phi_initial(i)));
+%         elseif phi_np1_km1_j(i)-phi_initial(i)<1
+%             y2(i)=S_initial(i)-(rho_i/rho_br)*one_over_phi(i)*(1-P_s)*S_initial(i)...
+%                 *(phi_np1_km1_j(i)-phi_initial(i))+one_over_phi(i)*S_source(i);
+%             SinI(i)=one_over_one_minus_phi(i)*(SinI(i)*(1-phi_initial(i))-S_initial(i)*P_s*(phi_np1_km1_j(i)-phi_initial(i)));
+%         else
+%             y2(i)=S_initial(i)-(rho_i/rho_br)*one_over_phi(i)*S_initial(i)...
+%                 *(phi_np1_km1_j(i)-phi_initial(i))+one_over_phi(i)*SinI(i)*(phi_np1_km1_j(i)-phi_initial(i))...
+%                 +one_over_phi(i)*S_source(i);
+%         end
+%     end
+
+    if BCoc==1
+        y2(end)=y2(end)+D_s_mod(end)*S_bottom;
+    else
+        a2(end)=1+D_s_mod(end);
+    end
+
+
+    %Solve for S
+    S_np1_k_j=Thomas_Trid(a2,b2,c2,y2');
+    
+    S_np1_km1_j=S_np1_k_j;
+    
+    %% Appending value to matrix to check for convergence
+    S_evolve=[S_evolve S_np1_k_j];
+    T_evolve=[T_evolve T_np1_k_j];
+    phi_evolve=[phi_evolve phi_np1_km1_j];
+    
+    %% Reassign T vector to use in next k iteration
+    T_np1_km1_j=T_np1_k_j;
+    phi_np1_k_j=phi_np1_km1_j;
+    % tolerance tests
+    if counter==1;
+        SErr=SErr;
+        TErr=TErr;
+        PhiErr=PhiErr;
+    else
+        TErr=max(abs(T_evolve(:,counter)-T_evolve(:,counter-1)));
+        PhiErr=max(abs(phi_evolve(:,counter)-phi_evolve(:,counter-1)));
+        SErr=max(abs(S_evolve(:,counter)-S_evolve(:,counter-1)));
+    end;
+end
+
+
+
